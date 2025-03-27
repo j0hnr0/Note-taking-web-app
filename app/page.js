9477482c5ff3e3b99b288299213640c0a49ce544
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-03-28 00:03:56 +0800

    in progress login form

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <section className="bg-custom-neutral-100 h-screen flex justify-center items-center">
+      <div className="max-w-[540px] mx-auto p-12 bg-white rounded-xl filter drop-shadow-[0_8px_12px_rgba(240,240,240,0.6)] border-[1px] border-custom-neutral-200">
+        <div className="flex justify-center items-center">
+          
+        </div>
+      </div>
+    </section>
   );
 }
