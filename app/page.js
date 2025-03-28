eb044a4233ba9714894f93f3bce36f7253312c64
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-03-28 19:41:13 +0800

    added Logo and Welcome Note

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+import localFont from 'next/font/local'
+import Logo from "./_components/logo";
+
+const inter = localFont({
+  src: '../public/fonts/inter/static/Inter_18pt-Bold.ttf',
+  display: 'swap',
+})
+
 export default function Home() {
   return (
     <section className="bg-custom-neutral-100 h-screen flex justify-center items-center">
-      <div className="max-w-[540px] mx-auto p-12 bg-white rounded-xl filter drop-shadow-[0_8px_12px_rgba(240,240,240,0.6)] border-[1px] border-custom-neutral-200">
+      <div className="w-full max-w-[540px] mx-auto p-12 bg-white rounded-xl filter drop-shadow-[0_8px_12px_rgba(240,240,240,0.6)] border-[1px] border-custom-neutral-200">
         <div className="flex justify-center items-center">
-          
+          <Logo/>
+        </div>
+        <div className="text-center mt-4">
+          <h1 className={`${inter.className} antialiased font-bold text-2xl text-custom-neutral-950`}>
+            Welcome to Note
+          </h1>
         </div>
       </div>
     </section>
