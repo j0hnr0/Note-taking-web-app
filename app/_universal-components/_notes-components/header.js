0ca3f26b327ef5f1a3da1783ac2021a144aaf6bf
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-05-08 22:08:47 +0800

    Notes list component

--- app/_universal-components/_notes-components/header.js
+++ app/_universal-components/_notes-components/header.js
@@ -1,10 +1,16 @@
+import SettingSvg from "../_svg-components/setting-svg";
 import Search from "./search";
 
 export default function Header() {
   return (
     <header className="pl-8 pr-10 py-[18.5px] border-b-[1px] border-b-custom-neutral-200 w-full flex justify-between items-center">
-      <h1 className="inter font-bold text-2xl text-custom-neutral-950">All Notes</h1>
-      <Search />
+      <h1 className="inter font-bold text-2xl text-custom-neutral-950">
+        All Notes
+      </h1>
+      <div className="flex justify-end items-center gap-6 w-2/4">
+        <Search />
+        <SettingSvg />
+      </div>
     </header>
   );
 }
---
+++ app/_universal-components/_notes-components/notes-list.js
@@ -0,0 +1,5 @@
+export default function NotesList() {
+  return (
+    <div className="py-5 px-4 border-r-[1px] border-r-custom-neutral-200"></div>
+  );
+}
