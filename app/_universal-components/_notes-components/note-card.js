a15fe800173c323f1840bf5c58d6544d48e1c403
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-05-26 22:31:55 +0800

    Displaying user's notes

--- app/_universal-components/_notes-components/note-card.js
+++ app/_universal-components/_notes-components/note-card.js
@@ -1,15 +1,33 @@
-export default function NoteCard() {
+export default function NoteCard({ title, tags, date }) {
+  const formattedDate = new Date(date).toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+
   return (
     <div className="w-full p-2 rounded-md bg-custom-neutral-100">
       <h5 className="inter font-semibold text-base text-custom-neutral-950">
-        React Performance Optimization
+        {title}
       </h5>
 
-      <div className="mt-3 py-0.5 px-1.5 text-center rounded-sm bg-custom-neutral-200">
-        <span className="inter font-normal text-xs text-custom-neutral-950">
-          Dev
-        </span>
+      <div className="flex justify-start gap-1">
+        {tags &&
+          tags.map((tag, index) => (
+            <div
+              key={index}
+              className="w-min mt-3 py-0.5 px-1.5 text-center rounded-sm bg-custom-neutral-200"
+            >
+              <span className="inter font-normal text-xs text-custom-neutral-950 block">
+                {tag}
+              </span>
+            </div>
+          ))}
       </div>
+
+      <span className="mt-3 inter font-normal text-xs text-custom-neutral-700">
+        {formattedDate}
+      </span>
     </div>
   );
 }
--- app/_universal-components/_notes-components/notes-list.js
+++ app/_universal-components/_notes-components/notes-list.js
@@ -4,9 +4,35 @@ import { useSelector } from "react-redux";
 import Button from "./button";
 import EmptyMessage from "./empty-message";
 import UntitledNote from "./untitled-note";
+import { useAuth } from "@/app/contexts/auth-provider";
+import { useQuery } from "@tanstack/react-query";
+import NoteCard from "./note-card";
 
 export default function NotesList() {
   const isOpen = useSelector((state) => state.notes.isNoteEditorOpen);
+  const { user } = useAuth();
+
+  const {
+    data: notes,
+    isPending,
+    error,
+  } = useQuery({
+    queryKey: ["noteData"],
+    queryFn: async () => {
+      const response = await fetch(`/api/note/fetch?userId=${user.id}`);
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Failed to fetch notes");
+      }
+
+      return data;
+    },
+  });
+
+  if (isPending) {
+    return <h1>Loading...</h1>
+  }
 
   return (
     <div className="w-full max-w-[290px] py-5 px-8 border-r-[1px] border-r-custom-neutral-200 h-full">
@@ -17,12 +43,13 @@ export default function NotesList() {
         bgColor="bg-custom-blue-500"
         toggle="open"
       />
+      <div className="mb-4"></div>
 
-      {isOpen ? (
-        <UntitledNote />
-      ) : (
-        <EmptyMessage message="You don’t have any notes yet. Start a new note to capture your thoughts and ideas." />
-      )}
+      {isOpen && <UntitledNote />}
+
+      {notes ? notes.map((note) => (
+        <NoteCard key={note.id} title={note.title} tags={note.tags} date={note.updatedAt} />
+      )) : <EmptyMessage message="You don’t have any notes yet. Start a new note to capture your thoughts and ideas." />}
     </div>
   );
 }
