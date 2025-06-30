fc2a06870619f6217c330b0231c571a7a0c16a92
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-06-30 16:35:54 +0800

    search api route

---
+++ app/api/note/search/route.js
@@ -0,0 +1,32 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
+import { searchAllNotes } from "@/app/lib/note-service";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const query = searchParams.get("query") || "";
+
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user?.id) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const userId = session.user.id;
+
+    const notes = await searchAllNotes({ userId, query });
+
+    return NextResponse.json(notes, { status: 200 });
+  } catch (error) {
+    if (error.message === "User not Found") {
+      return NextResponse.json({ message: "User not found." }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: error.message || "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
