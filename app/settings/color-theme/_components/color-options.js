72b1e35356c0a4dc6b9d8230ae7076de6e9bebec
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-07-07 22:11:08 +0800

    color-theme

--- app/settings/color-theme/_components/color-options.js
+++ app/settings/color-theme/_components/color-options.js
@@ -1,3 +1,5 @@
+import Theme from "./theme";
+
 export default function ColorOptions() {
   return (
     <form className="flex flex-col h-full py-5 px-6 w-full max-w-[562px] border-r-[1px] border-r-custom-neutral-200">
@@ -7,6 +9,8 @@ export default function ColorOptions() {
       <small className="mt-1 inter font-normal text-sm text-custom-neutral-700">
         Choose your color theme:
       </small>
+
+      <Theme  />
     </form>
   );
 }
