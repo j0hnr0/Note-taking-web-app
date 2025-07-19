a327bb5f9e1472fc8f9ff8f1bea1637c7cf53e2a
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-07-19 20:50:48 +0800

    theme options functionality

--- app/settings/color-theme/_components/color-options.js
+++ app/settings/color-theme/_components/color-options.js
@@ -1,32 +1,69 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import ApplyChangesBtn from "../../_components/apply-changes-btn";
 import TitleSubtitle from "../../_components/title-subtitle";
 import DarkSvg from "../_svg-components/dark-svg";
 import LightSvg from "../_svg-components/light-svg";
 import SystemSvg from "../_svg-components/system-svg";
 import Theme from "./theme";
+import { useTheme } from "next-themes";
 
 export default function ColorOptions() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    setSelectedTheme(theme);
+  }, [theme]);
+
+  if (!mounted) {
+    return null;
+  }
+
+  function handleTheme(e) {
+    e.preventDefault();
+    setTheme(selectedTheme)
+  }
+
   return (
-    <form className="flex flex-col h-full py-5 px-6 w-full max-w-[562px]">
+    <form
+      onSubmit={handleTheme}
+      className="flex flex-col h-full py-5 px-6 w-full max-w-[562px]"
+    >
       <TitleSubtitle title="Color Theme" subtitle="Choose your color theme:" />
 
       <Theme
         svg={LightSvg}
         title="Light Mode"
         subtitle="Pick a clean and classic light theme"
-        name="light-mode"
+        name="theme-selection"
+        value="light"
+        checked={selectedTheme === "light"}
+        onChange={(e) => setSelectedTheme(e.target.value)}
       />
       <Theme
         svg={DarkSvg}
         title="Dark Mode"
         subtitle="Select a sleek and modern dark theme"
-        name="dark-mode"
+        name="theme-selection"
+        value="dark"
+        checked={selectedTheme === "dark"}
+        onChange={(e) => setSelectedTheme(e.target.value)}
       />
       <Theme
         svg={SystemSvg}
         title="System"
         subtitle="Adapts to your device’s theme"
-        name="system-mode"
+        name="theme-selection"
+        value="system"
+        checked={selectedTheme === "system"}
+        onChange={(e) => setSelectedTheme(e.target.value)}
       />
 
       <ApplyChangesBtn text="Apply Changes" />
--- app/settings/color-theme/_components/theme.js
+++ app/settings/color-theme/_components/theme.js
@@ -1,7 +1,22 @@
-export default function Theme({ svg: Svg, title, subtitle, name }) {
+export default function Theme({
+  svg: Svg,
+  title,
+  subtitle,
+  name,
+  value,
+  checked,
+  onChange,
+}) {
   return (
     <label className="mt-4 w-full max-w-[528px] rounded-xl border-[1px] border-custom-neutral-200 p-4 flex justify-between items-center cursor-pointer has-[:checked]:bg-custom-neutral-100">
-      <input type="radio" name={name} className="peer sr-only" />
+      <input
+        type="radio"
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+        className="peer sr-only"
+      />
 
       <div className="flex justify-start items-center gap-4">
         <div className="rounded-xl bg-white border-[1px] border-custom-neutral-200 p-2">
