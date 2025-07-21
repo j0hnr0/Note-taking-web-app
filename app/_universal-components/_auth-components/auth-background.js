ed9b4392df4a4791784b4851a4a1bce929236a84
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-07-21 23:39:56 +0800

    dark mode style

--- app/_universal-components/_auth-components/auth-background.js
+++ app/_universal-components/_auth-components/auth-background.js
@@ -1,11 +1,11 @@
 export default function AuthBackground({ children }) {
   return (
     <section
-      className="bg-custom-neutral-100 h-screen flex justify-center items-center
+      className="bg-custom-neutral-100 dark:bg-custom-neutral-700 h-screen flex justify-center items-center
         max-custom-sm:px-5"
     >
       <div
-        className="w-full max-w-[540px] mx-auto p-12 bg-white rounded-xl filter drop-shadow-[0_8px_12px_rgba(240,240,240,0.6)] border-[1px] border-custom-neutral-200
+        className="w-full max-w-[540px] mx-auto p-12 bg-white dark:bg-custom-neutral-950 rounded-xl filter drop-shadow-[0_8px_12px_rgba(240,240,240,0.6)] dark:drop-shadow-none border-[1px] border-custom-neutral-200 dark:border-custom-neutral-800
               max-custom-md:px-8
               max-custom-sm:py-10 max-custom-sm:px-4"
       >
