867b4f49e73292acfda6f3012d543efaf371ef47
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-08-28 00:03:21 +0800

    forgot password api

---
+++ app/api/auth/forgot-password/route.js
@@ -0,0 +1,20 @@
+import { sendResetPasswordLink } from "@/app/lib/user-service";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { email } = await request.json();
+
+    const user = await sendResetPasswordLink({ email });
+
+    return NextResponse.json(
+      { message: "If an account exist, we sent an email" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: error.message || "Something went wrong." },
+      { status: 500 }
+    );
+  }
+}
